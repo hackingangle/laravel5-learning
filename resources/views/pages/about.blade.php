b36c56b5071f3b065b4f4e033ed557f4b2067278
hackingangle <hackingangle@gmail.com>
2015-10-20 16:34:34 +0800

    passing data to views

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About</title>
+</head>
+<body>
+    <h1>Hi, {{ $name }}</h1>
+    <p>To be a full stack programmer.To be a full stack programmer.To be a full stack programmer.To be a full stack programmer.To be a full stack programmer.</p>
+</body>
+</html>
